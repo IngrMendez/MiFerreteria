310631faa20d93af38580c17f05736aeb47157c2
IngrMendez <Dell@INGRIDGUTIERREZ>
2021-11-22 20:44:04 -0600

    Nuevos Cambios Proyecto

--- Ferreteria/Ferreteria/Ferreteria.BL/Contexto.cs
+++ Ferreteria/Ferreteria/Ferreteria.BL/Contexto.cs
@@ -19,10 +19,17 @@ namespace Ferreteria.BL
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            Database.SetInitializer(new DatosdeInicio());
         }
 
 
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
-    }
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Orden> Ordenes { get; set; }
+        public DbSet<OrdenDetalle> OrdenDetalle { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        
+        }
 }
